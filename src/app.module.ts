0953f74341b29390e96003b7e5144d804483128f
“LayyarVzz” <1486234558@qq.com>
2025-10-21 23:25:24 +0800

    优化项目结构

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,8 @@ import { EmbeddingModule } from './embedding/embedding.module';
 import { VectorStoreModule } from './vector-store/vector-store.module';
 import { RagModule } from './rag/rag.module';
 import { ConfigModule } from '@nestjs/config';
-import { IngestModule } from './ingest/ingest.module';
+import { IngestModule } from './docs/ingest/ingest.module';
+import { UploadModule } from './docs/upload/upload.module';
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { IngestModule } from './ingest/ingest.module';
     RagModule,
     ConfigModule.forRoot(),
     IngestModule,
+    UploadModule,
   ],
   controllers: [],
   providers: [],
---
+++ src/docs/upload/upload.controller.ts
@@ -0,0 +1,10 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { UploadService } from './upload.service';
+import { CreateUploadDto } from './dto/create-upload.dto';
+import { UpdateUploadDto } from './dto/update-upload.dto';
+
+@Controller('upload')
+export class UploadController {
+  constructor(private readonly uploadService: UploadService) { }
+
+}
