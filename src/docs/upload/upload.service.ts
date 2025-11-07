7f0f645cbd92f021f873769cace60a78d1cd00c4
“LayyarVzz” <1486234558@qq.com>
2025-11-07 23:42:11 +0800

    完善upload模块

--- src/docs/upload/upload.service.ts
+++ src/docs/upload/upload.service.ts
@@ -1,15 +1,50 @@
 import { Injectable, Logger } from '@nestjs/common';
+import { existsSync, unlinkSync } from 'fs';
+import { join } from 'path';
 
 @Injectable()
 export class UploadService {
     private readonly logger = new Logger(UploadService.name);
 
     echoFile(file: Express.Multer.File) {
-        this.logger.log(`文件上传成功: ${file.originalname}`);
+        if (existsSync(file.path)) {
+            this.logger.warn(`文件已存在，新文件将会覆盖旧文件: ${file.path}`);
+        } else {
+            this.logger.log(`文件上传成功: ${file.originalname}`);
+
+        }
         return {
             message: '文件上传成功',
             originalname: file.originalname,
             filename: file.filename,
+            mimetype: file.mimetype,
+        }
+    }
+
+    dleFile(filetype: string, filename: string) {
+        const filePath = join('./uploads', filetype, filename);
+        if (existsSync(filePath)) {
+            try {
+                unlinkSync(filePath);
+                this.logger.log(`文件删除成功: ${filePath}`);
+                return {
+                    message: '文件删除成功',
+                    filename,
+                    filePath,
+                    filetype,
+                };
+            } catch (error) {
+                this.logger.error(`文件删除失败: ${filePath}`, error);
+                throw new Error('文件删除失败');
+            }
+        } else {
+            this.logger.warn(`尝试删除不存在的文件: ${filePath}`);
+            return {
+                message: '尝试删除不存在的文件',
+                filename,
+                filePath,
+                filetype,
+            };
         }
     }
 }
