e404267f213b75d528c2ad8a0af2c6eea5e21240
“LayyarVzz” <1486234558@qq.com>
2025-11-18 22:01:36 +0800

    修改app.module.ts里的模块使用。

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,15 +3,14 @@ import { RagModule } from './rag/rag.module';
 import { ConfigModule } from '@nestjs/config';
 import { IngestModule } from './docs/ingest/ingest.module';
 import { UploadModule } from './docs/upload/upload.module';
-import { ParseDocModule } from './docs/parse-doc/parse-doc.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    //RagModule,
+    RagModule,
     IngestModule,
     UploadModule,
-    ParseDocModule],
+  ],
   controllers: [],
   providers: [],
 })
